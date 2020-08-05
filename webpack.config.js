3011f5e8b162cfacf840855c15ba50254282608f
sogoagain <imyong0@gmail.com>
2020-08-06 00:42:56 +0900

    Config [#12] 빌드 환경을 구성한다

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle-[hash].js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'index.html'),
+  })],
   module: {
     rules: [
       {
